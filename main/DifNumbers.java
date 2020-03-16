a891adf4cb7176f5c1e863ac278f1b2be30d0929
Alex Romaniuk <alaster.rom@gmail.com>
2020-03-16 18:37:15 +0200

    moved 1 file / change 8 line

---
+++ main/DifNumbers.java
@@ -0,0 +1,16 @@
+public class DifNumbers {
+
+   public static void main(String[] args) {
+      int num1 = 5, num2 = 15; 
+	  int dif;
+      dif = num1 - num2;
+      System.out.println("Dif of these numbers: " + dif);
+   }
+   
+   public static void main(String[] args) {
+      int num1 = 3, num2 = 18; 
+	  int del;
+      del = num1 / num2;
+      System.out.println("Del of these numbers: " + del);
+   }
+}
